467108b40f6e7508ebac7ca9bbb9e37e29444cec
ski.com <100414605+ski-dot-com@users.noreply.github.com>
2025-09-28 10:01:42 +0900

    [fix] 修正

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Chord, Note, Distance } from "tonal"
 import moji                      from "moji"
+const transpose=Distance.transpose as ((note: string, interval: string) => string) & ((note: string) => ((interval: string) => string))
 
 export const translateType = (_type:string) => {
   const notes = [0, 0, 0, null, null, null, null]
@@ -8,14 +9,14 @@ export const translateType = (_type:string) => {
   type = type.replace(/ 　/g,    "")
   type = type.replace(/[＃♯]/g,  "#")
   type = type.replace(/[♭ｂ]/g, "b")
-  let tension
+  let tension:string[]=[]
 
-  const tensionRegex = /[\(（](.+)[\)）]/
-  const tensionMatch = type.match(tensionRegex)
-  if (tensionMatch) {
-    tension = tensionMatch[1].replace(/[\s　]+/g, "").split(/[,，]/)
-    type = type.replace(tensionRegex, "")
+  const tensionRegex = /[\(（](.+)[\)）]/g
+  const tensionMatches = type.matchAll(tensionRegex)
+  for (let tensionMatch of tensionMatches) {
+    tension = tension.concat(tensionMatch[1].replace(/[\s　]+/g, "").split(/[,，]/))
   }
+  type = type.replace(tensionRegex, "")
 
   const parseType = (regex:RegExp) => {
     if (type.match(regex)) {
@@ -42,10 +43,10 @@ export const translateType = (_type:string) => {
   switch (true) {
     case parseType(/^5/):  notes[1] = null; break
     case parseType(/^6/):  notes[3] = -1;   break
+    case parseType(/^13/): notes[6] = 0;
+    case parseType(/^11/): notes[5] = 0;
+    case parseType(/^9/):  notes[4] = 0;
     case parseType(/^7/):  notes[3] = 0;    break
-    case parseType(/^9/):  notes[3] = 0;    notes[4] = 0; break
-    case parseType(/^11/): notes[3] = 0;    notes[4] = 0; notes[5] = 0; break
-    case parseType(/^13/): notes[3] = 0;    notes[4] = 0; notes[5] = 0; notes[6] = 0; break
   }
   // sus
   switch (true) {
@@ -63,15 +64,15 @@ export const translateType = (_type:string) => {
   }
   // M
   switch (true) {
+    case parseType(/(M|[Mm]aj|△|Δ)13/): notes[6] = 0; 
+    case parseType(/(M|[Mm]aj|△|Δ)11/): notes[5] = 0; 
+    case parseType(/(M|[Mm]aj|△|Δ)9/):  notes[4] = 0; 
     case parseType(/(M|[Mm]aj|△|Δ)7/):  notes[3] = 1; break
-    case parseType(/(M|[Mm]aj|△|Δ)9/):  notes[3] = 1; notes[4] = 0; break
-    case parseType(/(M|[Mm]aj|△|Δ)11/): notes[3] = 1; notes[4] = 0; notes[5] = 0; break
-    case parseType(/(M|[Mm]aj|△|Δ)13/): notes[3] = 1; notes[4] = 0; notes[5] = 0; notes[6] = 0; break
   }
   // dim
   switch (true) {
-    case parseType(/^(dim|o)7/): notes[1] -= 1; notes[2] -= 1; notes[3] = -1; break
-    case parseType(/^(dim|o)/):  notes[1] -= 1; notes[2] -= 1; break
+    case parseType(/^(dim|o)7/):  notes[3] = -1; 
+    case parseType(/^(dim|o)/):  if(notes[1]!==null)notes[1] -= 1; if(notes[2]!==null)notes[2] -= 1; break
   }
   // tension
   if (tension) type += tension.join("")
@@ -103,26 +104,41 @@ export const translateType = (_type:string) => {
   return notes
 }
 
+const transposer = (note:string, interval:number) => {
+  let tmp=Note.midi(note)
+  if(tmp===null){
+    return false
+  }
+  return Note.fromMidi(tmp + interval)
+}
 
-const transposer = (note, interval) => Note.fromMidi(Note.midi(note) + interval)
-
-const buildChord = (root, baseNotes, translator) => {
-  const notes = []
+const buildChord = (root:string, translator:(number|null)[]) => {
+  const notes:string[] = []
   const chord13 = Chord.notes(root, "13") // 13コードを基準にして音を足したり減らしたりする
-  chord13.splice(5, 0, Distance.transpose(root, "M11")) // tonal の 13コードは 11th が omit されている
+  chord13.splice(5, 0, transpose(root, "M11")) // tonal の 13コードは 11th が omit されている
 
   for (let i = 0; i < 7; i += 1) {
-    if (translator[i] !== null) notes.push(transposer(chord13[i], translator[i]))
+    let tmp=translator[i]
+    if (tmp !== null) {
+      let tmp_=transposer(chord13[i], tmp)
+      if(tmp_==false){
+        return false
+      }
+      notes.push(tmp_)
+    }
   }
   return notes
 }
 
-const chordTranslator = (root, type = "", baseKey = 3) => {
-  const baseNotes = Chord.notes(`${root}${baseKey}`, "M")
+const chord2notes = (root:string, type = "", baseRangeStart = 48) => {
   const translator = translateType(type)
   if (!translator) return false
-  const notes = buildChord(`${root}${baseKey}`, baseNotes, translator)
+  let tmp=Note.midi(root)
+  if(tmp===null){
+    return false
+  }
+  const notes = buildChord(Note.fromMidi(((tmp-baseRangeStart)%12+12)%12+baseRangeStart), translator)
   return notes
 }
 
-export default chordTranslator
+export default chord2notes
